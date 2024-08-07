c7f703509e6693d7594db6eb5d0c4bb8393e3750
DalerKhusainov <dalerkhusainov85@gmail.com>
2024-08-07 23:04:59 +0500

    configured custom styles in tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,52 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        100: "#FF9483",
+        200: "#FF4D30",
+        300: "#E6452B",
       },
+      gray: {
+        100: "#F8F8F8",
+        200: "#DFDFDF",
+        300: "#C6C6C6",
+        900: "#2D2D2D",
+      },
+      textBody: "#706F7B",
+      textBlack: "#010103",
+      buttonBlack: "#161616",
     },
+    screens: {
+      mobile: "540px",
+      tablet: "960px",
+      desktop: "1248px",
+    },
+    boxShadow: {
+      buttonPrimary: "0px 10px 15px 0px rgba(255, 83, 48, 0.35)",
+      buttonPrimaryHover: "0px 10px 15px 0px rgba(255, 83, 48, 0.5)",
+      testimonials: "0px 20px 40px 0px rgba(0, 0, 0, 0.08)",
+      cta: "0px 10px 40px 0px rgba(0, 0, 0, 0.1)",
+      faq: "0px 10px 15px 0px rgba(0, 0, 0, 0.1)",
+    },
+    fontSize: {
+      xs: ["16px", { lineHeight: "25.6px", letterSpacing: "-0.03em" }],
+      sm: ["18px", { lineHeight: "32.4px", letterSpacing: "-0.03em" }],
+      lg: ["22px", { lineHeight: "24px", letterSpacing: "-0.03em" }],
+      xl: ["24px", { lineHeight: "24px", letterSpacing: "-0.03em" }],
+      "2xl": ["30px", { lineHeight: "36px", letterSpacing: "-0.032em" }],
+      "3xl": ["42px", { lineHeight: "50.8px", letterSpacing: "-0.032em" }],
+      "4xl": ["52px", { lineHeight: "54.6px", letterSpacing: "-0.032em" }],
+    },
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+      rubik: "Rubik, sans-serif",
+    },
+    borderRadius: {
+      sm: "4px",
+      md: "8px",
+    },
+    extend: {},
   },
   plugins: [],
 };
